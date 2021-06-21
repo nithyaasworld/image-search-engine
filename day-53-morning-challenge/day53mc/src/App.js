bd194e6c4db326fadd93b4ebc9bd0274c1b5cc5b
Nithya <nithyaasworld@gmail.com>
2021-06-22 01:04:30 +0530

    first commit

---
+++ day-53-morning-challenge/day53mc/src/App.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import "./App.css";
+import Notification from './Notification';
+
+function App() {
+  let [showNotificatoin, setShowNotification] = useState(false);
+  let [message, setMessage] = useState("");
+  let [isError, setIsError] = useState(false);
+
+  const showNotificationMsg = () => {
+    setIsError(false);
+    setMessage("I am notification popup");
+    setShowNotification(true);
+  }
+  const showErrorPopup = () => {
+    setIsError(true);
+    setMessage("I am an error popup");
+    setShowNotification(true);
+  }
+  return (
+    <>
+    <Notification error={isError} text={message} showNotificatoin={showNotificatoin} setShowNotification={setShowNotification} />
+    <div className="button-container">
+      <div onClick={showNotificationMsg} class="button notification-message">Notification message</div>
+      <div onClick={showErrorPopup} class="button notification-alert">Notification alert</div>
+    </div>
+    </>
+  );
+}
+
+export default App;
